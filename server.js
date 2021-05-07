22573d3eae2cbd52326f78bd30dbd24ea8d7c741
Mohammad Altamimi <m98altamimi@gmail.com>
2021-05-07 16:27:18 +0300

    query

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express()
 const weather = require('./data/weather.json')
 const cors= require ('cors')
 require('dotenv').config()
+const superagent = require('superagent');
 
 
 const PORT=process.env.PORT || 8080
+const WEATHER_BIT_KEY=process.env.WEATHER_BIT_KEY
 
 
 app.use(cors());
@@ -13,9 +15,14 @@ app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
+
 app.get('/weather', function (req, res) {
-    const arrayOfData= weather.data.map(data => new Weather(data)) 
-    res.send(arrayOfData)
+const weatherBitUrl=`https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${38.123}&lon=${-78.543}`;
+superagent.get(weatherBitUrl).then (data=>{
+    return res.send(data.body)
+}).catch(console.error);
+    
+    
 })
 
 class Weather {
@@ -29,3 +36,4 @@ app.listen(PORT)
 
 // note: you will get an error if you use { in arrow functions like line 10} without any return statement 
 //The idea is that we are targeting the json file which we required earlier then we specified the data part of it then we mapped through it creating an instance [new Weather] at every object element .
+// note that 
